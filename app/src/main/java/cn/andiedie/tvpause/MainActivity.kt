2d4332b5d2bf709414f63b4219b30efccb234785
Andie <zchangan@163.com>
2019-02-04 00:21:41 +0800

    :construction: Test layout

--- app/src/main/java/cn/andiedie/tvpause/MainActivity.kt
+++ app/src/main/java/cn/andiedie/tvpause/MainActivity.kt
@@ -1,12 +1,30 @@
 package cn.andiedie.tvpause
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, Service::class.java)
+        intent.action = Service.ACTION.Initial
+        startService(intent)
+
+        findViewById<Button>(R.id.stop).setOnClickListener {
+            Intent(this, Service::class.java)
+            intent.action = Service.ACTION.Stop
+            startService(intent)
+        }
+
+        findViewById<Button>(R.id.resume).setOnClickListener {
+            Intent(this, Service::class.java)
+            intent.action = Service.ACTION.Resume
+            startService(intent)
+        }
     }
 }
