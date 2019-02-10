0e32ed0befd361690853b2dcc4900fdd0f5f08d1
Andie <zchangan@163.com>
2019-02-10 23:32:46 +0800

    :boom: Breaking change

--- app/src/main/java/cn/andiedie/tvpause/Const.kt
+++ app/src/main/java/cn/andiedie/tvpause/Const.kt
@@ -1,6 +1,7 @@
 package cn.andiedie.tvpause
 
 object Const {
-    const val SETTING_NAME = "SETTING_NAME"
-    const val PAUSE_ON_CALL = "PAUSE_ON_CALL"
+    const val SETTING_NAME = "cn.andiedie.TVPause.SETTING_NAME"
+    const val PAUSE_ON_CALL = "cn.andiedie.TVPause.PAUSE_ON_CALL"
+    const val VOLUME_BACKUP = "cn.andiedie.TVPause.VOLUME_BACKUP"
 }
--- app/src/main/java/cn/andiedie/tvpause/MainActivity.kt
+++ app/src/main/java/cn/andiedie/tvpause/MainActivity.kt
@@ -3,153 +3,135 @@ package cn.andiedie.tvpause
 import android.Manifest
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
-import android.telephony.TelephonyManager
 import android.util.Log
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
-import org.greenrobot.eventbus.EventBus
-import org.greenrobot.eventbus.Subscribe
-import org.greenrobot.eventbus.ThreadMode
 
-private const val PERMISSIONS_REQUEST_READ_PHONE_STATE = 123
+private const val PERMISSIONS_REQUEST = 852
 private const val TAG = "TVPause.MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private var permission = false
-    private var link = false
-    private var pauseOnCall = false
     private lateinit var statusView : TextView
-    private var receiver: PhoneStateReceiver? = null
+    private lateinit var checkBox: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, Service::class.java)
-        intent.action = Service.ACTION.INITIAL
-        startService(intent)
-
         statusView = findViewById(R.id.status)
 
         findViewById<Button>(R.id.pause).setOnClickListener {
-            Intent(this, Service::class.java).also {
-                it.action = Service.ACTION.PAUSE
+            Intent(this, TVService::class.java).also {
+                it.action = TVService.PAUSE
                 startService(it)
             }
         }
 
         findViewById<Button>(R.id.resume).setOnClickListener {
-            Intent(this, Service::class.java).also {
-                it.action = Service.ACTION.RESUME
+            Intent(this, TVService::class.java).also {
+                it.action = TVService.RESUME
                 startService(it)
             }
         }
 
-        val checkBox = findViewById<CheckBox>(R.id.pauseOnCall)
-        pauseOnCall = getSharedPreferences(Const.SETTING_NAME, Context.MODE_PRIVATE).getBoolean(Const.PAUSE_ON_CALL, false)
-        checkBox.isChecked = pauseOnCall
+        checkBox = findViewById<CheckBox>(R.id.pauseOnCall)
+        val setting = getSharedPreferences(Const.SETTING_NAME, Context.MODE_PRIVATE)
+        checkBox.isChecked = setting.getBoolean(Const.PAUSE_ON_CALL, false)
 
         checkBox.setOnClickListener{
-            pauseOnCall = (it as CheckBox).isChecked
-            updateStatus()
-            val setting = getSharedPreferences(Const.SETTING_NAME, Context.MODE_PRIVATE)
+            val pauseOnCall = (it as CheckBox).isChecked
             setting.edit().putBoolean(Const.PAUSE_ON_CALL, pauseOnCall).apply()
             if (pauseOnCall) {
-                registerPhoneStateReceiver()
+                startPhoneStateReceiverService()
             } else {
-                unregisterPhoneStateReceiver()
+                stopPhoneStateReceiverService()
             }
         }
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            Log.d(TAG, "No phone state permission")
-            ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.READ_PHONE_STATE),
-                PERMISSIONS_REQUEST_READ_PHONE_STATE)
+
+        val permissions = mutableListOf<String>(
+            Manifest.permission.READ_PHONE_STATE
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            permissions += Manifest.permission.FOREGROUND_SERVICE
+        }
+        if (!hasPermissions(permissions)) {
+            Log.d(TAG, "Permissions missing")
+            ActivityCompat.requestPermissions(this, permissions.toTypedArray(), PERMISSIONS_REQUEST)
         } else {
-            Log.d(TAG, "Phone state permission granted")
+            Log.d(TAG, "Permissions granted")
             permission = true
-            if (pauseOnCall) {
-                registerPhoneStateReceiver()
-            }
         }
 
         updateStatus()
-        EventBus.getDefault().register(this)
+    }
+
+    private fun hasPermissions(permissions: List<String>): Boolean {
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (grantResults.isEmpty()) return
         when (requestCode) {
-            PERMISSIONS_REQUEST_READ_PHONE_STATE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d(TAG, "Request Phone state permission success")
-                    permission = true
-                    updateStatus()
-                    registerPhoneStateReceiver()
+            PERMISSIONS_REQUEST -> {
+                var flag = true
+                for (result in grantResults) {
+                    if (result != PackageManager.PERMISSION_GRANTED) {
+                        flag = false
+                        break
+                    }
+                }
+                permission = if (flag) {
+                    Log.d(TAG, "Request permissions success")
+                    true
                 } else {
-                    Log.d(TAG, "Request phone state permission failed")
-                    permission = false
-                    updateStatus()
+                    Log.d(TAG, "Request permissions failed")
+                    false
                 }
             }
         }
-    }
-
-    override fun onBackPressed() {
-        moveTaskToBack(false)
-    }
-
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    fun onMessageEvent(connectedEvent: ConnectedEvent) {
-        Log.d(TAG, "Connection status received: ${connectedEvent.connected}")
-        link = connectedEvent.connected
         updateStatus()
+
     }
 
-    private fun registerPhoneStateReceiver() {
-        if (receiver == null) {
-            Log.d(TAG, "Register PhoneStateReceive")
-            receiver = PhoneStateReceiver()
-            IntentFilter(TelephonyManager.ACTION_PHONE_STATE_CHANGED)
-                .also { registerReceiver(receiver, it) }
+    private fun startPhoneStateReceiverService() {
+        Intent(this, PhoneStateReceiverService::class.java).also {intent ->
+            intent.action = PhoneStateReceiverService.START
+            startService(intent)
         }
     }
 
-    private fun unregisterPhoneStateReceiver() {
-        if (receiver != null) {
-            Log.d(TAG, "Unregister PhoneStateReceive")
-            unregisterReceiver(receiver)
-            receiver = null
+    private fun stopPhoneStateReceiverService () {
+        Intent(this, PhoneStateReceiverService::class.java).also {intent ->
+            intent.action = PhoneStateReceiverService.STOP
+            startService(intent)
         }
     }
 
     private fun updateStatus() {
-        statusView.text = resources.getString(
-            R.string.status,
-            if (permission) "已授权" else "未授权",
-            if (link) "已连接" else "未连接"
-        )
-        val color : Int = if (pauseOnCall) {
-            if (permission && link) {
-                R.color.colorSuccess
-            } else {
-                R.color.colorFail
-            }
+        statusView.text = if (permission) "已授权" else "未授权"
+        val color : Int = if (permission) {
+            R.color.colorSuccess
         } else {
-            if (link) {
-                R.color.colorSuccess
-            } else {
-                R.color.colorFail
-            }
+            R.color.colorFail
         }
+        checkBox.isEnabled = permission
         statusView.setBackgroundResource(color)
+
+        if (checkBox.isChecked) {
+            startPhoneStateReceiverService()
+        }
     }
 
 }
